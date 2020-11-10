2f91c03354c9cd19f29ac1836114fb67035497e4
Christina Cortland <crosalie@live.unc.edu>
2020-11-10 14:36:07 -0500

    Comment out disallow-doctype-decl with TODO

--- archivesspace_checker.rb
+++ archivesspace_checker.rb
@@ -26,7 +26,8 @@ class ArchivesspaceChecker < Sinatra::Base
 
   # Disable a bunch of stuff in parser to prevent XXE vulnerabilities
   parser_options = Saxon::Processor.default.to_java.getUnderlyingConfiguration.parseOptions
-  parser_options.add_parser_feature("http://apache.org/xml/features/disallow-doctype-decl", true)
+  # TODO: figure out how to allow EADs with doctype declaration without weakening security? (AS-56)
+  # parser_options.add_parser_feature("http://apache.org/xml/features/disallow-doctype-decl", true)
   parser_options.add_parser_feature("http://xml.org/sax/features/external-general-entities", false)
   parser_options.add_parser_feature("http://xml.org/sax/features/external-parameter-entities", false)
 
